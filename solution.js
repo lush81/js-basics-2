db234b661c7e4f625745e8560dd04c5ddf8369da
lush81_Shapovalenko <lush@i.ua>
2015-01-30 23:25:20 +0200

    Solutions to Exercises js-basics-2 from Shapovalenko

--- solution.js
+++ solution.js
@@ -9,7 +9,18 @@ function isEven(number) {
   // For any other number N, its evenness is the same as N - 2.
   // Define a recursive function isEven corresponding to this description. The function should accept a number parameter and return a Boolean.
   // Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
-}
+ if(number<0){
+  number=-number;
+ }
+ if (number ===0){
+  return true;
+ }
+ if (number ===1){
+ return false;
+ }
+ number = number-2;
+return isEven(number);
+ }
 
 function pascal(row, col) {
   // Write a procedure that computes the elements of Pascal's triangle by means of a recursive process.
@@ -21,29 +32,54 @@ function pascal(row, col) {
   //
   // pascal(4, 2)
   // // → 6
-}
+ if(col<=row){
+  if(col===0 || row===col){
+   return 1;
+  } else{
+   return pascal(row-1, col-1) + pascal(row-1, col);
+  }
+ }
+}  
 
 function sqrt(x) {
   // Square roots by Newton's method http://en.wikipedia.org/wiki/Newton%27s_method
   // Also you can look at the problem description in SICP[1.1.7] http://newstar.rinet.ru/~goga/sicp/sicp.pdf
   // You should write a function that takes a number and computes square root
   // You can build solution based on functions structure defined below or you can make up your own
+
+var guess = 1;
+return sqrtIter(guess, x);
 }
 
 function sqrtIter(guess, x) {
+ if (isGoodEnough(guess, x)){
+  return guess;
+ } else{
+  return sqrtIter(improve(guess, x), x);
+ }
 }
 
 function improve(guess, x) {
+ return average(guess, x/guess);
 }
 
 function average(x, y) {
-}
+ return (x+y)/2;
+ }
 
 function isGoodEnough(guess, x) {
+
+ if(Math.abs(square(guess)-x)<1e-20){
+  return true;
+ }else{
+  return false;
+ }
 }
 
 function square(x) {
+ return x*x;
 }
+
 
 module.exports.isEven = isEven;
 module.exports.pascal = pascal;
